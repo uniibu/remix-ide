14ad4b86aa8b660c041d129a5403baad6e2733aa
davidekete <eketedavid@gmail.com>
2025-09-18 11:51:51 +0100

    docs: change plugin list to grid

--- docs/conf.py
+++ docs/conf.py
@@ -30,7 +30,8 @@ import datetime
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['myst_parser', 'sphinx_copybutton', 'sphinx.ext.autosectionlabel'] # required for sphinx v3.0.0
+extensions = ['myst_parser', 'sphinx_copybutton', 'sphinx.ext.autosectionlabel', 'sphinx_design'] # required for sphinx v3.0.0
+myst_enable_extensions = ["colon_fence"]
 copybutton_prompt_text = r">>> |\$ "
 copybutton_prompt_is_regexp = True
 autosectionlabel_prefix_document = True
